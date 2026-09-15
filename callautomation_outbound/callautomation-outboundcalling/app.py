20bdffb0cbe518257d9f16b62a2957acb08d0e8d
ayushgupta030789 <ayush.gupta030789@gmail.com>
2026-09-15 18:30:19 +0530

    Update app.py

--- callautomation_outbound/callautomation-outboundcalling/app.py
+++ callautomation_outbound/callautomation-outboundcalling/app.py
@@ -87,6 +87,8 @@ def outbound_call_handler():
                                                                     cognitive_services_endpoint=COGNITIVE_SERVICES_ENDPOINT,
                                                                     source_caller_id_number=source_caller)
     app.logger.info("Created call with connection id: %s", call_connection_properties.call_connection_id)
+    print(create_call_result.call_connection.call_connection_id)
+    print(create_call_result.call_connection_properties.call_connection_state)
     return redirect("/")
 
 
